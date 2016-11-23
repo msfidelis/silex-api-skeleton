2fd2e4f419f848f8dcea647af0a965411c36b6e5
Matheus Fidelis <msfidelis@outlook.com>
2016-11-23 00:12:48 -0200

    Criei um arquivo de configuração em JSON. Adicionei um ambiente Docker dentro do projeto

--- web/app/Configs/Config.php
+++ web/app/Configs/Config.php
@@ -7,13 +7,21 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\DBAL\Query\QueryBuilder;
+use App\Classes\Configs\Config;
 use App\System\Model;
 
 Request::enableHttpMethodParameterOverride();
 
+//Seta a Static de Config
+$jsonFile = __DIR__ . "/Config.json";
+$jsonContent = file_get_contents($jsonFile);
+Config::$config = (object) json_decode($jsonContent);
+
 $app = new Application();
 $app['debug'] = True;
 
+
+//Configura o Twig. Mover isso para o config.json
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   "twig.path" => __DIR__ . "../../src/Views",
   "twig.form.templates"=>array('form_div_layout.html.twig',"form/form_div_layout.twig"),
@@ -21,20 +29,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   )
 );
 
+//Configura o Banco de Dados.
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
-      'driver'    => 'pdo_pgsql',
-      'host'      => 'PostgreSQL',
-      'dbname'    => 'pgdb',
-      'user'      => 'pguser',
-      'password'  => 'pgpass'
+      'driver'    => Config::$config->db->driver,
+      'host'      => Config::$config->db->host,
+      'dbname'    => Config::$config->db->schema,
+      'user'      => Config::$config->db->user,
+      'password'  => Config::$config->db->password,
     ),
 ));
 
+//Instancia um QueryBuilder genérico. Será utilizado na classe Model
 Model::$db = $app['db'];
 Model::$query = new QueryBuilder($app['db']);
 
 require __DIR__ . "/Routes.php";
-
-
 return $app;
---
+++ web/src/Classes/Configs/Config.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Classes\Configs;
+/**
+* Manipula os configs da aplicação
+* @author Matheus Scarpato Fidelis
+* @email matheus.scarpato@superlogica.com
+*/
+class Config {
+
+  public static $config;
+  public static $iniFile;
+
+}
