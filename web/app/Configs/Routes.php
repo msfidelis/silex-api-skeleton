c624d507e21d90ec0485cdde95afc6a4a84698a2
Matheus Fidelis <msfidelis@outlook.com>
2017-01-27 10:49:56 -0200

    elimiei a pasta app

--- web/app/Configs/Routes.php
+++
@@ -1,3 +0,0 @@
-<?php
-$app->mount("/", new App\Controllers\IndexController());
-$app->mount("/api",new App\Controllers\ApiController());
--- web/index.php
+++ web/index.php
@@ -1,4 +1,3 @@
 <?php
-require_once __DIR__."/app/Configs/Config.php";
-
+require_once __DIR__."/src/Configs/Bootstrap.php";
 $app->run();
